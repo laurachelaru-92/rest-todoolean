b561f5833c55e4b1a537667baacf5a1a94ae1ea2
Laura Chelaru <laura.a.chelaru@gmail.com>
2020-09-28 15:45:40 +0200

    chiamate GET e POST ok

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $.ajax({
   "url": "http://157.230.17.132:3010/todos",
   "method": "GET",
   "success": function(data) {
-    printElements(data);
+    printEl(data);
   },
   "error": function(error) {
     alert("Error!");
@@ -13,7 +13,7 @@ $.ajax({
 });
 
 // Funzione che, preso un array, ne stampa ogni "text" nel template
-function printElements(genericArray) {
+function printEl(genericArray) {
   for(var i = 0; i < genericArray.length; i++) {
     var source = $("#insert-templ").html();
     var template = Handlebars.compile(source);
@@ -28,26 +28,32 @@ function printElements(genericArray) {
 // Evento al click su "send"
 $("header").on("click", "#send", function(){
   var value = $("header input").val();
-  alert(value);
+  if(value != "") {
+    addEl(value);
+  }
+  $("header input").val("");
 });
 // Evento all'invio
 $("header").keyup("header input", function(e){
   if(e.which == 13) {
     var value = $("header input").val();
-    alert(value);
+    if(value != "") {
+      addEl(value);
+    }
+    $("header input").val("");
   }
 });
 
-// Funzione che esegue chiamata ajax POST
-function addElement(input) {
+// Funzione che esegue chiamata ajax POST prendendo una stringa input
+function addEl(input) {
   $.ajax({
     "url": "http://157.230.17.132:3010/todos",
     "data": {
-      "text": "Prepara la pizza"
+      "text": input
     },
     "method": "POST",
     "success": function(data) {
-      printElements([data]);
+      printEl([data]);
     }
   });
 }
